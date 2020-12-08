5cb077366a35a48c37f062ad02cc5a6e5a63b91e
Gary <Gary@PENGUINSROCK>
2020-12-07 21:34:26 -0600

    Add T1010 Application Window Discovery

---
+++ Discovery/T1010-ApplicationWindowDiscovery/T1010-ApplicationWindowDiscovery/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace T1010_ApplicationWindowDiscovery
+{
+    class T1010
+    {
+        static void Main(string[] args)
+        {
+            var RunningProcs = System.Diagnostics.Process.GetProcesses();
+
+            Console.WriteLine("---------- T1010 Application Window Discovery ----------");
+            Console.WriteLine("");
+
+            foreach (Process TitledProcess in RunningProcs)
+            {
+                if (TitledProcess.MainWindowTitle.ToString() != "")
+                {
+                    Process T1010Processes = TitledProcess;
+                    var ID = T1010Processes.Id;
+                    var Name = T1010Processes.ProcessName;
+                    var Title = T1010Processes.MainWindowTitle;
+                    Console.WriteLine("++++++++++");
+                    Console.WriteLine(string.Format("Process Name : {0}", Name));
+                    Console.WriteLine("     " + Title);
+                    Console.WriteLine("");
+                }
+            }
+        }
+    }
+}
